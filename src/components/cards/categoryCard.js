c3e45742c28c016824f2964cebe8b3f06376c4da
sir3n-sn <sir3n.sn@gmail.com>
2018-03-01 19:33:42 +0300

    [Feature] Added view Categories on click

--- src/components/cards/categoryCard.js
+++ src/components/cards/categoryCard.js
@@ -1,26 +1,36 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import CategorySubMenu from '../menu/menu.js';
+import ViewCategoryModal from '../modals/viewCategory'
 
 class CategoryCard extends React.Component {
+    state = {
+        open: false,
+    }
+    
+    show = dimmer => this.setState({ dimmer, open: true })
+    close = () => this.setState({ open: false })
+
     render() {
         const { category } = this.props;
+        const { open } = this.state;
         return (
-    <div className="ui card">
-        <div className="extra header" style={{marginLeft: "70%" }}>
-            <CategorySubMenu redirectRecipes= {this.props.redirectRecipes} id={category['id']} category={category}/> 
-        </div>
-        <div className="content">
-            <div className="header">{category['Recipe Category Name']}</div>
-            <div className="meta"><em>Brief description</em></div>
-            <div className="description">
-            <p>{category['Recipe Category Detail']}</p>
+            <div className="ui card" onClick={() => this.show('blurring')}>
+                <ViewCategoryModal open={open} close={ this.close } category={ category } id={category['id']}/>
+                <div className="extra header" style={{marginLeft: "70%" }}>
+                    <CategorySubMenu redirectRecipes= {this.props.redirectRecipes} id={category['id']} category={category}/> 
+                </div>
+                <div className="content">
+                    <div className="header">{category['Recipe Category Name']}</div>
+                    <div className="meta"><em>Brief description</em></div>
+                    <div className="description">
+                    <p>{category['Recipe Category Detail']}</p>
+                    </div>
+                </div>  
+                <div className="extra footer">
+                <i className="info icon"></i>Created: {category['Date Created'].slice(0,22)}..
+                </div>
             </div>
-        </div>  
-        <div className="extra footer">
-        <i className="info icon"></i>Created: {category['Date Created'].slice(0,22)}..
-        </div>
-    </div>
         )
     }
 
---
+++ src/components/modals/viewCategory.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Header, Modal } from 'semantic-ui-react'
+import PropTypes from 'prop-types';
+
+
+class ViewCategoryModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    loading: false,
+    open: props.open, 
+    };
+  }
+
+  close = () => {
+    this.props.close()
+  }
+
+  render() {
+    const { dimmer, open } = this.props;
+    const { category } = this.props;
+    return (
+      <Modal dimmer={dimmer} open={open} onClose={ this.close } size='tiny'>
+      <Modal.Header>{category['Recipe Category Name']}</Modal.Header>
+      <Modal.Content>
+        <Modal.Description>
+          <Header>Details</Header> {category['Recipe Category Detail']}
+          <div>
+            <br/>
+            <em>Created on</em>: {category['Date Created']}
+            <div>
+            <br/>
+            <em>Modified on</em>: {category['Date Created']}
+            </div>
+          </div>
+        </Modal.Description>
+      </Modal.Content>
+    </Modal>
+    )
+  }
+};
+
+ViewCategoryModal.propTypes = {
+  open: PropTypes.bool.isRequired,
+  close: PropTypes.func.isRequired,
+  id: PropTypes.number.isRequired,
+  category: PropTypes.object.isRequired
+};
+
+
+export default ViewCategoryModal
